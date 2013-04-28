d620e8b53713e68862b7fe87d7add36d95566232
Kevin Krauss <earlkrauss@gmail.com>
2013-04-27 19:58:58 -0700

    hubot hook to be used for iming users about the build

---
+++ lib/big_tuna/hooks/hubot.rb
@@ -0,0 +1,80 @@
+module BigTuna
+  class Hooks::Hubot < Hooks::Base
+    NAME = "hubot"
+
+    def build_fixed(build, config)
+      enqueue(build_info(build, "great-commit"), config)
+    end
+
+    def build_still_fails(build, config)
+      enqueue(build_info(build, "broken-spec"), config)
+    end
+
+    def build_failed(build, config)
+      enqueue(build_info(build, "broken-spec"), config)
+    end
+
+    class Job
+      require 'net/http'
+      require 'net/https'
+      require 'uri'
+
+      def initialize(message, config)
+        @message = message
+        @config = config
+      end
+
+      def perform
+        connect
+        if @config[:request_type] == "post"
+          post
+        else
+          get
+        end
+      end
+
+      def connect
+        @uri = URI.parse(@config[:server_uri] + @message[:status])
+        @http = Net::HTTP.new(@uri.host, @uri.port)
+      end
+
+      def get
+        path = "/hubot/#{@message[:status]}?user=#{@message[:user]}&project=#{@message[:project]}&branch=#{@message[:branch]}&tag=#{@message[:tag]}"
+        headers = {'Accept' => 'plain/text',
+                   'Content-Type' => 'plain/text'}
+        response, data = @http.get(path, headers)
+
+        { :message => @message, :response => response, :data => data }
+      end
+
+      def post
+        request = Net::HTTP::Post.new(@uri.request_uri)
+        request.set_form_data({ :status => @message[:status],
+                                :user => @message[:user],
+                                :project => @message[:project],
+                                :branch => @message[:branch],
+                                :tag => @message[:tag],
+                                :build_stdout => @message[:build_stdout]
+                              })
+        response = @http.request(request)
+      end
+    end
+
+    private
+      def enqueue(message, config)
+        Delayed::Job.enqueue(Job.new(message, config))
+      end
+
+      def build_info(build, status)
+        failing_build_parts = build.parts.select { |part| part.status == "status_build_part_failed" }
+        failure_stdout = failing_build_parts.collect { |fail_part| fail_part.output.collect{|out| out.stdout + out.stderr } }.join("\n")
+        { :status => status,
+          :user =>"#{build.email}",
+          :project => "#{build.project.name}",
+          :branch =>"#{build.project.vcs_branch}",
+          :tag => "#{build.commit}",
+          :build_stdout => "#{failure_stdout}"
+        }
+      end
+  end
+end
